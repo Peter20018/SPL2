af65fc898b6a6409a65b41e7648aaac69c945ca3
Peter20018 <161wit02@o365.htl-leoben.at>
2018-04-18 11:49:05 +0200

    Ungerade Zahlen

--- Zahlenreihen.py
+++ Zahlenreihen.py
@@ -13,5 +13,9 @@ for i in range(1,n+1):
 
 for i in range(1,n+1):
     if(i % 2 == 0):
-        print(i, end ="  gerade ")
+        print(i, end =" gerade ")
+        
+for i in range(1,n+1):
+    if(i % 2 == 1):
+        print(i, end =" ungerade ")
     
