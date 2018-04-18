2b3780d2d9d794866c343e288d404e83ae4788cd
Peter20018 <161wit02@o365.htl-leoben.at>
2018-04-18 09:45:57 +0200

    Erstes Python programm

--- hello-world.py
+++ hello-world.py
@@ -3,5 +3,10 @@ name = input("Wie ist dein Name?")
 print("Schön dich zu treffen ", name ,". Dein Name ist ", len(name) ,"Zeichen lang")
 
 print("Noch eine Frage. ")
-alter = int(input("Wie alt bist du den? "))
-print("Aha, sehr interressant ", name, " du wirst in einem Jahr ", alter+1, " Jahre alt sein.")
+alter = input("Wie alt bist du den? ") 
+alter = int(alter)
+if(alter > 100):
+    print("Das ist wohl ein Scherz - oder? ")
+else:
+    print("Aha, sehr interressant ", name, " du wirst in einem Jahr ", alter+1, " Jahre alt sein.")
+print("Tschüssi...")
