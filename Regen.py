f455645c9785b669bd40e962f10d534322fadb9c
Peter20018 <161wit02@o365.htl-leoben.at>
2018-04-18 10:36:54 +0200

    Regen abfrage

---
+++ Regen.py
@@ -0,0 +1,21 @@
+# Regen.py
+
+regen = True
+
+eingabe1 = input("Hast du einen Regenschirm? Ja oder nein. ")
+
+if(eingabe1 == "ja"):
+    print("Gut das du einen Schirm dabei hast Geh jetzt nach draußen")
+    regen = False
+
+while(regen):
+    print("Warte bis der Regen aufhört... ")
+    eingabe = input("Regnet es nocht?")
+
+    if (eingabe == "nein"):
+        regen = False
+    elif (eingabe == "ja"):
+        print("Es regnet noch immer... ")
+    else:
+        print ("bitte nur ja oder nein eingeben")
+    print("Jetzt regnet es nicht mehr ")
